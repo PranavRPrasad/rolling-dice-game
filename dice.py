c1f412ebdeefb8894e855be40346ff8dea65c4ac
Pranav R Prasad <pranavrp.pr@gmail.com>
2019-10-13 01:33:57 +0530

    Update dice.py

--- dice.py
+++ dice.py
@@ -5,7 +5,7 @@ max=6
 roll_again="yes"
 
 while roll_again=="yes" or roll_again=="y":
-	print "Rolling the dices..."
+	print "Rolling the dices(1 to 6)..."
 	print "The values are......"
 	print random.randint(min,max)
 	
